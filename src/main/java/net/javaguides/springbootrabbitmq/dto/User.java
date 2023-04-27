1690fcec31229270669ff0ed009e2dc76d0e19a4
Nurs <nursultanamanzholov91@gmail.com>
2023-04-27 15:26:07 +0600

    Add MessageJsonController

--- src/main/java/net/javaguides/springbootrabbitmq/dto/User.java
+++ src/main/java/net/javaguides/springbootrabbitmq/dto/User.java
@@ -1,5 +1,11 @@
 package net.javaguides.springbootrabbitmq.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Data
 public class User {
 
     private int id;
